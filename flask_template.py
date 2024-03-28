fff918d17dc4621461480f5cfb6835d2e784cdd7
Bonsu <99491901+bonsututu@users.noreply.github.com>
2024-03-27 23:09:33 -0400

    Add files via upload

---
+++ flask_template.py
@@ -0,0 +1,45 @@
+import random
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# List of poems
+poems = [
+    {
+        "title": "The Road Not Taken",
+        "author": "Robert Frost",
+        "text": "Two roads diverged in a yellow wood, And sorry I could not travel both..."
+    },
+    {
+        "title": "Ozymandias",
+        "author": "Percy Bysshe Shelley",
+        "text": "I met a traveller from an antique land Who said: Two vast and trunkless legs of stone..."
+    },
+    {
+        "title": "Daffodils",
+        "author": "William Wordsworth",
+        "text": "I wandered lonely as a cloud That floats on high o'er vales and hills..."
+    },
+    {
+        "title": "Sonnet 18",
+        "author": "William Shakespeare",
+        "text": "Shall I compare thee to a summer's day? Thou art more lovely and more temperate..."
+    },
+    {
+        "title": "The Raven",
+        "author": "Edgar Allan Poe",
+        "text": "Once upon a midnight dreary, while I pondered, weak and weary..."
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', poems=poems)
+
+@app.route('/show_another_poem')
+def show_another_poem():
+    random_poem = random.choice(poems)
+    return render_template('another_poem.html', poem=random_poem)
+
+if __name__ == '__main__':
+    app.run(debug=True)
